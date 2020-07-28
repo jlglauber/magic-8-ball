51fd93c0aa159a417b943d11744ea80cac35636c
jamie.g <jlglauber@hotmail.com>
2020-07-28 12:02:22 -0400

    Initial add

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+ $("#answer").hide();
+    magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+ 
+        $("#answer").text( answer );
+ 
+        console.log(question);
+        console.log(answer);
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+    };
+ 
+    var onClick = function() {
+        $("#answer").hide();
+		$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+		//wait half a second before showing prompt
+		setTimeout(
+			function()	{
+				//show prompt
+                $("#8ball").effect("shake");
+        var question = prompt("Ask a yes or no question!");
+        magic8Ball.askQuestion(question);
+			}, 500);
+		$("#answer").fadeIn(4000);
+    };
+	
+    $("#questionButton").click( onClick );
+ 
+});
